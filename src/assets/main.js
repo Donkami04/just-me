927039eec1048dba6bde68210b2779f5de91f30d
donkami <camilo.munerac@gmail.com>
2022-12-14 13:09:52 -0500

    finaly

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,45 +1,54 @@
-const API = 'https://api.escuelajs.co/api/v1/products'
+// const fetch = require('node-fetch') 
+// import fetch from 'node-fetch'
+const API = 'https://youtube-v31.p.rapidapi.com/playlistItems?playlistId=PL_c9BZzLwBRLVh9OdCBYFEql6esA6aRsi&part=snippet&maxResults=4';
 
 const contenido = document.getElementById('contenedor');
 const emergente = document.querySelector(' .emergente');
 
 const options = {
 	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '9ba8647aabmsh4e9da4a0a04c5b2p1c4f8cjsn267debaf3e43',
-		'X-RapidAPI-Host': 'weather-embed.p.rapidapi.com'
-	}
+    headers: {
+        'X-RapidAPI-Key': '9ba8647aabmsh4e9da4a0a04c5b2p1c4f8cjsn267debaf3e43',
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+    }
 };
 
 async function fetchData(urlApi){
     const response = await fetch(urlApi, options);
     const data = await response.json();
     return data;
+
 };
 
 
 // Esta funcion se llama automaticamente
 (async () => {
     try{
-        const products = await fetchData(API);
+        const videos = await fetchData(API);
+        console.log(videos.items[0].snippet.thumbnails.high.url)
         let view = `
-        ${products.map(product => `
+        
+        ${videos.items.map(video => `
+        <a href="https://www.youtube.com/watch?v=${video.snippet.resourceId.videoId}"target="_blank">
         <div class="group relative">
             <div
                 class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                <img src="${product.images}" alt="aaaa" class="w-full">
+                <img src="${video.snippet.thumbnails.high.url}" alt="aaaa" class="w-full">
             </div>
             <div class="mt-4 flex justify-between">
                 <h3 class="text-sm text-gray-700">
                 <span aria-hidden="true" class="absolute inset-0"></span>
-                ${product.description}
+                ${video.snippet.title}
                 </h3>
             </div>
-        </div>`).slice(0,8).join('')}
+        </div>
+        </a>`
+        ).slice(0,8).join('')}
         `;
         contenido.innerHTML = view;
     } catch(error){
-        console.log(error)
+        console.log(stack.error)
+        throw new Error(error)
         
     }
 })();
